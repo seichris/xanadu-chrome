1a99245825d5cb4a417e1fa1fb31f91fd063ef51
seichris <cerujurasa@gmail.com>
2020-05-08 22:02:47 +0200

    tailwind works

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # xanadu-chrome
 
 Thanks to [3box](https://3box.io/) and [this react chrome addon how-to](https://itnext.io/create-chrome-extension-with-reactjs-using-inject-page-strategy-137650de1f39).
+
+
+
+1. get current URL
+2. search for space of this current URL
+3. if space exists...
+... open the thread
+4. put components there, with coordinates.
+
+
+
+
+1. metamask auth
+2. add comment
+3. save comment
+
+
+
+settings page?
+- show all comments yes/no
+
+profile page?
--- src/content.js
+++ src/content.js
@@ -1,32 +1,37 @@
 /*global chrome*/
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./content.css";
+import Frame, { FrameContextConsumer }from 'react-frame-component';
+import "./styles/inside.css";
+import "./styles/outside.css";
+import './styles/tailwind.css';
+import './styles/index.css';
 
 class Main extends React.Component {
-    render() {
-        return (
-            <Frame head={[<link type="text/css" rel="stylesheet" href={chrome.runtime.getURL("/static/css/content.css")} ></link>]}>
-              <FrameContextConsumer>
-               {
-               // Callback is invoked with iframe's window and document instances
-                   ({document, window}) => {
-                      // Render Children
-                      return (
-                         <div className={'my-extension'}>
-                            <h1>Hello world - My first Extension</h1>
-                         </div>
-                      )
-                   }
-                }
-               </FrameContextConsumer>
-            </Frame>
-        )
-    }
+  render() {
+    return (
+        <Frame head={[<link type="text/css" rel="stylesheet" href={chrome.runtime.getURL("/static/css/content.css")} ></link>]}>
+          <FrameContextConsumer>
+           {
+           // Callback is invoked with iframe's window and document instances
+              ({ document, window }) => {
+                // Render Children
+                return (
+                   <div className={'main p-8'}>
+                      <p className={'text-lg text-right text-gray-700'}>this should render new-comment-input</p>
+                      <p className="text-left text-gray-900">try tailwind</p>
+                   </div>
+                );
+              }
+            }
+           </FrameContextConsumer>
+        </Frame>
+    );
+  }
 }
 
 const app = document.createElement('div');
-app.id = "root";
+app.id = "very-unique-naming-root";
 document.body.appendChild(app);
 ReactDOM.render(<Main />, app);
 

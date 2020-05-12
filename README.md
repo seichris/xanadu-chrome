319c90a551226e502c73477f4128a2ff17f12190
seichris <cerujurasa@gmail.com>
2020-05-12 14:56:21 +0200

    draggable

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Chrome extension to highlight the div which is hovered by  https://stackoverflow.com/questions/4445102/google-chrome-extension-highlight-the-div-that-the-mouse-is-hovering-over
+Then append a child to that div https://stackoverflow.com/questions/20432517/chrome-extension-append-element-to-another-element-by-class-name
+
+
+How to remove the hash names from manifest, wihout ejecting
+https://github.com/facebook/create-react-app/issues/3805
--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Draggable from 'react-draggable';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const blankState = {
+  comment: "",
+  rating: "",
+  deltaPosition: {
+        x: 0, y: 0
+      }
+};
+
+
+class App extends React.Component {
+
+  state = blankState;
+
+    handleChange = event => {
+      this.setState(Object.assign({ [event.target.name]: event.target.value }));
+    };
+
+    handleDrag = (e, ui, deltaPosition) => {
+        const {x, y} = this.state.deltaPosition;
+        this.setState(Object.assign({ deltaPosition: { x: x + ui.deltaX, y: y + ui.deltaY } }));
+      };
+
+    onControlledDragStop = (e, position) => {
+        const {x, y} = position;
+        this.setState(Object.assign({ deltaPosition: {x, y}}));
+      };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <Draggable
+           axis="both"
+           handle=".handle"
+           defaultPosition={{x: 0, y: 0}}
+           position={this.state.deltaPosition}
+           //grid={[25, 25]}
+           scale={1}
+           onStart={this.handleStart}
+           onDrag={this.handleDrag}
+           onStop={this.onControlledDragStop}>
+           <div>
+             <div className="handle">what's your summary?</div>
+             <div>Put text here</div>
+             <div>x: {this.state.deltaPosition.x.toFixed(0)}, y: {this.state.deltaPosition.y.toFixed(0)}</div>
+           </div>
+         </Draggable>
+      </div>
+    );
+  }
 }
 
 export default App;
